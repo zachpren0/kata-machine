9b0158efb153639874666b08415541f04510f510
zachpren0 <77454945+zachpren0@users.noreply.github.com>
2023-09-19 09:38:43 -0700

    nice

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,45 @@
 export default class MinHeap {
     public length: number;
+    private data: number[];
 
-    
 
     constructor() {
+        this.data = [];
+        this.length = 0
     }
 
     insert(value: number): void {
 
-}
+    }
     delete(): number {
 
-}
+    }
+
+    private heapifyUp(idx: number): void{
+        if (idx === 0 ){
+            return;
+        }
+        const p = this.parent(idx)
+        const parentV = this.data[p]
+        const v = this.data[idx];
+
+        if (parentV > v){
+            this.data[idx] = parentV;
+            this.data[p] = v;
+            this.heapifyUp(p)
+        }
+
+    }
+
+    private parent(idx: number): number {
+        return Math.floor((idx-1)/2);
+    }
+
+    private leftChild(idx: number): number {
+        return idx * 2 + 1;
+    }
+
+    private rightChild(idx: number): number {
+        return idx * 2 + 2
+    }
 }
